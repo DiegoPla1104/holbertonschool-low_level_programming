9a3bcc35f151dc2f0a48491b82f8f7ccb9f438e3
DiegoPla1104 <diegopla1104@gmail.com>
2023-06-15 06:23:22 -0700

    i

---
+++ hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ *main - a string is printed using write
+ *
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19")
+return (0);
+}
