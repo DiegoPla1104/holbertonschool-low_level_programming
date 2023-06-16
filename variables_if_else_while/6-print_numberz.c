dfea77894cacacc28365664a3ebc3904a8e1c7e2
DiegoPla1104 <diegopla1104@gmail.com>
2023-06-16 05:32:53 -0700

    commit

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Alphabet
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+int main(void)
+{
+	char ipa;
+
+	for (ipa = '0' ; ipa <= '9' ; ipa++)
+	{
+		putchar(ipa);
+	}
+	putchar('\n');
+	return (0);
+}
