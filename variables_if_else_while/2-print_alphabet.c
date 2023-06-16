6b17addedef2de2c14de5a3e8854f1a05ac14d52
DiegoPla1104 <diegopla1104@gmail.com>
2023-06-15 18:53:00 -0700

    m

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Alphabet
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+int main(void)
+{
+	char ipa;
+
+	for (ipa = 'a' ; ch <= 'z' ; ch++)
+	{
+	putchar(ipa);
+	}
+	return (0);
+}
