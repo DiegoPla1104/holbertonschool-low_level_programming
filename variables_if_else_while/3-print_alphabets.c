b374ba993e1f12c923c1b2adb00be2566bb1afa3
DiegoPla1104 <diegopla1104@gmail.com>
2023-06-15 19:41:06 -0700

    hola primer commit

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Alphabet
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+int main(void)
+{
+	char ipa;
+
+	if (ipa = 'a' ; ipa <= 'z' ; ipa++)
+	{
+		putchar(ipa);
+	}
+	putchar('\n');
+	return (0);
+}
