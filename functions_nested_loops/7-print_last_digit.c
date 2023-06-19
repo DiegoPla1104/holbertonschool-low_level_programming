eda82f31e28fb0b11964e556a361b43bbb09e78b
DiegoPla1104 <diegopla1104@gmail.com>
2023-06-18 18:29:38 -0700

    commit

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *print_last_digit - Prints the last digit of a number
+ *@r: is a variable
+ *Return: On Success (0)
+ */
+int print_last_digit(int r)
+{
+	int n = (r / 10);
+
+	_putchar(n);
+
+	return (0);
+}
