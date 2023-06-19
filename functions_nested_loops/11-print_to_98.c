48681a445889d072b30f380e53d936df1d95d897
DiegoPla1104 <diegopla1104@gmail.com>
2023-06-19 10:42:14 -0700

    commit

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -18,7 +18,7 @@ void print_to_98(int n)
 
 			printf("%d, ", uupa);
 
-			if (n == 98)
+			else if (n == 98)
 
 			printf("%d\n", uupa);
 		}
@@ -31,7 +31,7 @@ void print_to_98(int n)
 
 			printf("%d, ", uupa);
 
-			if (n == 98)
+			else if (n == 98)
 
 			printf("%d\n", uupa);
 
