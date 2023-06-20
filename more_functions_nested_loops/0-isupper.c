5486ec8cc9968b04415679b251c2039ccede0825
DiegoPla1104 <diegopla1104@gmail.com>
2023-06-20 05:07:35 -0700

    commit

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *_islower - Detects which letter is lowercase
+ *@c: is a variable
+ *Return: On Success (0)
+ */
+int _islower(int c)
+{
+		int aipa = c;
+
+		if (aipa > 64 && aipa < 91)
+		{
+			return (1);
+		}
+		else
+		{
+			return (0);
+		}
+}
