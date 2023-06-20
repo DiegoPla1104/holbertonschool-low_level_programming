3df2f8e7b28701bb67676126b0ada5187c7db417
DiegoPla1104 <diegopla1104@gmail.com>
2023-06-20 07:24:16 -0700

    commit

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -9,18 +9,14 @@ void more_numbers(void)
 {
 	char aepa;
 	char aipa;
-	int a;
 
-	for (a = 1; a <= 10; a++)
+	for (aepa = '0'; aepa <= '9' ; aepa++)
 	{
-		for (aepa = '0'; aepa <= '9' ; aepa++)
-		{
-			_putchar(aepa);
-			for (aipa = '0'; aipa <= '4'; aipa++)
-			{
-				_putchar(aepa + 10);
-			}
-		}
-		_putchar('\n');
+		_putchar(aepa);
 	}
+	for (aipa = '0'; aipa <= '4'; aipa++)
+	{
+		_putchar(aipa + '9' + '1');
+	}
+		_putchar('\n');
 }
