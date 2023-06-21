e784a8398d23dfcf48a6e46417ed1201d47e3f3f
DiegoPla1104 <diegopla1104@gmail.com>
2023-06-21 09:37:42 -0700

    commit

--- pointers_arrays_strings/1-swap.c
+++ pointers_arrays_strings/1-swap.c
@@ -1,17 +1,14 @@
 #include "main.h"
 
 /**
- * swap_int - Exchanges the values of two integers
+ * swap_int - Exchanges the values of two integers with the help of temp
  *@a: is a number
  *@b: is anoter number
  */
 
 void swap_int(int *a, int *b)
 {
-
-	*a = 98;
-	*b = 42;
-
-	*a = &b;
-	*b = &a;
+	int c = *a;
+	*a = *b;
+	*b = c;
 }
