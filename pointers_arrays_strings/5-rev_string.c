6c03f9639e3e38c91d35bf23013dd56b11ef4708
DiegoPla1104 <diegopla1104@gmail.com>
2023-06-22 09:14:15 -0700

    commit

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *print_rev - Prints a string in reverse
+ *@s: Is the pointer of example
+ */
+void print_rev(char *s)
+{
+	int u = 0;
+
+	while (s[u] != '\0')
+	{
+		u++;
+	}
+	for (; u >= 0; u--)
+	{
+		_putchar(s[u]);
+	}
+	_putchar('\n');
+}
