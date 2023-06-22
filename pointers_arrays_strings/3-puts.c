a790091b20ed1b7144558be91d70d830a096f9ce
DiegoPla1104 <diegopla1104@gmail.com>
2023-06-22 06:02:53 -0700

    commit

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ *_puts - Is used as a function that prints the string
+ *
+ *@str: is a string
+ *Return: Always 0 (Success)
+ */
+void _puts(char *str)
+{
+	_putchar(str[0]);
+}
