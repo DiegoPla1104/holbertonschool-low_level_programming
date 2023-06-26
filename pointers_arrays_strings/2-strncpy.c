1f3c6ef8209c20437286aa548096197d16019c74
DiegoPla1104 <diegopla1104@gmail.com>
2023-06-26 08:13:29 -0700

    commit

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -2,21 +2,22 @@
 /**
  *_strncpy - copies from one string to another
  *@dest: Destiny of the copy
- *@src: Source oof the copy
+ *@src: Source of the copy
  *@n: Limit of the copy
  *Return: Always 0 (Success)
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i;
+	int i = 0;
 
-	for (i = 0; i < n; i++)
+	while (i < n && src[i] != '\0')
 	{
 		dest[i] = src[i];
+		i++;
 	}
-	if (i < n)
+	while (i < n)
 	{
-		src[i++] = '\0';
+		dest[i] = '\0';
 		i++;
 	}
 	return (dest);
