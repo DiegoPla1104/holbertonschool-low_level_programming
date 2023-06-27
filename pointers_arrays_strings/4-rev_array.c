2f5cc8f0de7739990e86b193c47a00624288ba18
DiegoPla1104 <diegopla1104@gmail.com>
2023-06-26 18:31:19 -0700

    commit

--- pointers_arrays_strings/4-rev_array.c
+++ pointers_arrays_strings/4-rev_array.c
@@ -2,11 +2,21 @@
 /**
  *reverse_array - Reverses an array
  *
+ *@n: Is the number of elements in the array
  *
+ *@a: The array to be reversed
  *
- *
- *
- *
+ *Return: Void
  */
 void reverse_array(int *a, int n)
 {
+	int e;
+	int w;
+
+	for (e = n - 1; e >= n; e--)
+	{
+		w = a[n - 1 - e];
+		a[n - 1 - e] = a[e];
+		a[e] = w;
+	}
+}
