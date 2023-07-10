951417aae7f5907272357e37fcf1c46fe3e25edf
DiegoPla1104 <diegopla1104@gmail.com>
2023-07-10 06:17:47 -0700

    commit

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,19 +1,9 @@
-#ifndef
-#define dog
-struct User
+#ifndef dog
+#define dog_h
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
-
-int main(void)
-{
-	struct User dog;
-
-	dog.name = "Poppy";
-	dog.age = "3.5";
-	dog.owner = "Bob";
-	return (0);
-}
 #endif
