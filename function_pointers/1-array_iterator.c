3a2f1006959d0fe18b97ad08c265c4762a7c0227
DiegoPla1104 <diegopla1104@gmail.com>
2023-07-11 07:33:48 -0700

    commit

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,16 @@
+#include "function_pointers.h"
+/**
+ *array_iterator - Takes a function and runs it on each element of an array
+ *@array: Is the array to be examined
+ *@size: Determines the size of said array
+ *@action: Is a pointer to the function you need to use
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned long int count = 0;
+
+	for (; count < size; count++)
+	{
+		action(array[count]);
+	}
+}
