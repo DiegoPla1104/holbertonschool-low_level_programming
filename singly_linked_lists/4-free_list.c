96ae6babbd9747c7659cfa37fe134c4ac16bc7ee
DiegoPla1104 <diegopla1104@gmail.com>
2023-07-25 08:47:20 -0700

    feat: free_list

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ *free_list - Frees a list.
+ *@head: Is the head of the list.
+ *
+ *Return: the number of nodes.
+ */
+void free_list(list_t *head)
+{
+	list_t *tmp = NULL;
+
+	while (head != NULL)
+	{
+		tmp = head;
+		head = head->next;
+		free(tmp);
+	}
+}
