24c468db66e2043cdf2bdbd9f65177db4cbbd741
DiegoPla1104 <diegopla1104@gmail.com>
2023-07-26 16:43:46 -0700

    fix: Kernel style code error

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -1,9 +1,9 @@
 #include "lists.h"
 
 /**
- *print_dlistint - djfasdfjsodjsdjc
- *@h: fdjkdlkdmsodoksdm
- *Return: dklmcksdmfskldfkldmfldkmf
+ *print_dlistint - Prints all the elements of a list
+ *@h: Head of the list
+ *Return: The number of nodes
  */
 size_t print_dlistint(const dlistint_t *h)
 {
