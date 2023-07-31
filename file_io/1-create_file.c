7efaea58966f0a84ec54b6f2664199d378e63ea2
DiegoPla1104 <diegopla1104@gmail.com>
2023-07-31 10:24:37 -0700

    fix: Created file not empty

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -17,7 +17,7 @@ int create_file(const char *filename, char *text_content)
 	else
 	{
 		stor = open(filename, O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
-		if (stor != -1)
+		if (stor == -1)
 		{
 			if (text_content != NULL)
 			{
