9a72ab12cd75b051a929cfb56b280dbaecb1e359
DiegoPla1104 <diegopla1104@gmail.com>
2023-07-31 09:49:23 -0700

    feat: New features

---
+++ file_io/1-create_file.c
@@ -0,0 +1,40 @@
+#include "main.h"
+/**
+ *create_file - Creates a file with rw permissions
+ *@filename: The name of the file.
+ *@text_content: Is the content of the file.
+ *
+ *Return: 1 on success, -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int stor = 0;
+
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+	else
+	{
+		stor = open(filename, O_RDWR);
+		if (stor == -1)
+		{
+			creat(filename, S_IRUSR & S_IWUSR);
+			if (text_content != NULL)
+			{
+				write(STDIN_FILENO, text_content, strlen(text_content));
+			}
+			else
+			{
+				write(STDIN_FILENO, "", 1);
+			}
+			chmod(filename, 0600);
+			close(stor);
+		}
+		else
+		{
+			return (-1);
+		}
+	}
+	return (1);
+}
