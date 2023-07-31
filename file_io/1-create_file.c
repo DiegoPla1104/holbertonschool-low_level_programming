9375b481371ff963e40f9de1badea12abbf8e16d
DiegoPla1104 <diegopla1104@gmail.com>
2023-07-31 10:37:02 -0700

    fix: Read permissions

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -16,7 +16,7 @@ int create_file(const char *filename, char *text_content)
 	}
 	else
 	{
-		stor = open(filename, O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+		stor = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 		if (stor == -1)
 		{
 			if (text_content != NULL)
@@ -25,7 +25,7 @@ int create_file(const char *filename, char *text_content)
 			}
 			else
 			{
-				write(stor, "", 1);
+				write(stor, "", 0);
 			}
 			close(stor);
 		}
