174538ed6c67cdbfda839e5fbdcb44a63fe415cf
DiegoPla1104 <diegopla1104@gmail.com>
2023-08-01 10:10:53 -0700

    fix: Permission granting error

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -16,7 +16,7 @@ int create_file(const char *filename, char *text_content)
 	}
 	else
 	{
-		stor = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+		stor = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 		if (stor == -1)
 		{
 			if (text_content != NULL)
