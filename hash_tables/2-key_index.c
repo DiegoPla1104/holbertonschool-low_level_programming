8fae6043a3ddc81b3ff4753f9a9811312c651ce9
DiegoPla1104 <diegopla1104@gmail.com>
2023-08-03 18:18:50 -0700

    feat: Returns the key addres

---
+++ hash_tables/2-key_index.c
@@ -0,0 +1,12 @@
+#include "hash_tables.h"
+
+/**
+ *key_index - Returns the addres of a key
+ *@key: Key to be returned
+ *@size: Size of the hash table
+ *Return: The index of the key
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	return (hash_djb2(key) % size);
+}
